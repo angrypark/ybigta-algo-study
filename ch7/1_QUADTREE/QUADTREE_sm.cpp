8539ca815d231a7027884e39844d30ca83f6eac2
Semin Park <SPark9625@gmail.com>
2019-07-28 21:00:53 +0900

    add answer to QUADTREE

---
+++ ch7/1_QUADTREE/QUADTREE_sm.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <vector>
+
+class Solution
+{
+private:
+public:
+    std::string text, answer;
+    Solution()
+    {
+        std::getline(std::cin, text);
+    }
+
+    std::string solve(std::string str, bool root = false)
+    {
+        if (str.size() == 1) return str;
+
+        std::vector<std::string> buffer(4);
+
+        int offset = (str[0] == 'x') ? 1 : 0;
+        bool mixed = (str[0] == 'x') ? true : false;
+
+        for (int i = 0; i < 4; i++) {
+            if (str[i + offset] != 'x')
+                buffer[i] = str[i + offset];
+            else {
+                buffer[i] = solve(str.substr(i + offset));
+                offset += buffer[i].size() - 1;
+            }
+        }
+        std::string result = buffer[2] + buffer[3] + buffer[0] + buffer[1];
+        return mixed ? 'x' + result : result;
+    }
+};
+
+
+int main(int argc, const char* argv[])
+{
+    std::string line;
+    std::getline(std::cin, line);
+    int num_cases = std::stoi(line);
+
+    for (int i = 0; i < num_cases; i++) {
+        Solution sol;
+        auto answer = sol.solve(sol.text, true);
+        std::cout << answer << std::endl;
+    }
+    return 0;
+}
